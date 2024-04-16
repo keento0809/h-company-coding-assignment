483b51e7f7e4567b9e1bb3adda433883aa17ab15
KEENTO <handball0321@gmail.com>
2024-04-17 07:19:28 +0900

    Add: HomePageSection comoponent

---
+++ app/_components/HomePageSection.tsx
@@ -0,0 +1,12 @@
+import styles from "./HomePageSection.module.css";
+
+export const HomePageSection = () => {
+  return (
+    <main className={styles.wrapper}>
+      <div className={styles.container}>
+        <h2>TodoList</h2>
+        {/* TodoList component will be here */}
+      </div>
+    </main>
+  );
+};
