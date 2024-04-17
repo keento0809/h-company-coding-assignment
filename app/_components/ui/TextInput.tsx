50313491ad57bbd8a766716f1aff96cd86333521
KEENTO <handball0321@gmail.com>
2024-04-17 09:02:42 +0900

    Add: basic styles to TextInput component

--- app/_components/ui/TextInput.tsx
+++ app/_components/ui/TextInput.tsx
@@ -1,7 +1,15 @@
 import { ComponentProps } from "react";
+import styles from "@/app/_components/styles/textInput.module.css";
 
 type TextInputProps = ComponentProps<"input">;
 
 export const TextInput = ({ onChange, ...props }: TextInputProps) => {
-  return <input type="text" onChange={onChange} {...props} />;
+  return (
+    <input
+      type="text"
+      onChange={onChange}
+      {...props}
+      className={styles.textInput}
+    />
+  );
 };
