57d966b60cb3373424c83fc1064447a0d359ccfd
KEENTO <handball0321@gmail.com>
2024-04-18 07:47:56 +0900

    Add: feature of adding new task to TaskList

--- app/(home)/_components/NewTodoItemForm.tsx
+++ app/(home)/_components/NewTodoItemForm.tsx
@@ -1,13 +1,24 @@
 import { Button } from "@/app/_components/ui/button/Button";
 import { TextInput } from "@/app/_components/ui/input/TextInput";
 import styles from "@/app/(home)/styles/newTodoItemForm.module.css";
+import { FC } from "react";
 
-export const NewTodoItemForm = () => {
+type NewTodoItemFormProps = {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  taskTitle: string;
+};
+
+export const NewTodoItemForm: FC<NewTodoItemFormProps> = ({
+  onSubmit,
+  onChange,
+  taskTitle,
+}) => {
   return (
-    <form onSubmit={() => {}} className={styles.formContainer}>
+    <form onSubmit={onSubmit} className={styles.formContainer}>
       <div className={styles.formTitleSection}>
         <label htmlFor="taskTitle">Title</label>
-        <TextInput id="taskTitle" />
+        <TextInput id="taskTitle" onChange={onChange} value={taskTitle} />
       </div>
       <div className={styles.formActionButtons}>
         <Button text="Add" type="submit" />
