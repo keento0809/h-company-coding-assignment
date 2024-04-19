050c739090b92f7990a7b06dc92826620873e772
KEENTO <handball0321@gmail.com>
2024-04-19 09:34:06 +0900

    Add: TodoContext

---
+++ app/context/todoContext.tsx
@@ -0,0 +1,132 @@
+import { ReactNode, useState, createContext, useContext } from "react";
+
+export type Todo = {
+  id: string;
+  title: string;
+  isDone: boolean;
+};
+
+export type TodoType = "NEW" | "EDIT";
+
+type TodoContextType = {
+  todos: Todo[];
+  newTodoTitle: string;
+  editingTodo: Todo | null;
+  handleChangeTodoTitle: (
+    e: React.ChangeEvent<HTMLInputElement>,
+    todoType: TodoType
+  ) => void;
+  isModalOpen: boolean;
+  handleAddNewTodo: (e: React.FormEvent<HTMLFormElement>) => void;
+  handleOpenEditTodoModal: (id: string) => void;
+  handleToggleIsDone: (todo: Todo) => void;
+  handleUpdateTodo: (e: React.FormEvent<HTMLFormElement>) => void;
+  handleDeleteTodo: (id: string) => void;
+  closeModal: () => void;
+};
+
+const TodoContext = createContext<TodoContextType | null>(null);
+
+export const TodoProvider = ({ children }: { children: ReactNode }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+  const [editingTodo, setEditingTodo] = useState<Todo | null>(null);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleChangeTodoTitle = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    todoType: TodoType
+  ) => {
+    switch (todoType) {
+      case "NEW":
+        setNewTodoTitle(e.target.value);
+
+      case "EDIT":
+        setEditingTodo((prev) => {
+          if (prev) {
+            return { ...prev, title: e.target.value };
+          }
+          return null;
+        });
+
+      default:
+        return;
+    }
+  };
+
+  const handleAddNewTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (newTodoTitle.trim() === "") throw new Error("New task title is empty.");
+
+    const newTodo = {
+      id: String(todos.length + 1),
+      title: newTodoTitle,
+      isDone: false,
+    };
+
+    setTodos((prev) => [...prev, newTodo]);
+    setNewTodoTitle("");
+  };
+
+  const handleOpenEditTodoModal = (id: string) => {
+    setIsModalOpen(true);
+    const editTodo = todos.find((todo) => todo.id === id);
+    if (editTodo) {
+      setEditingTodo(editTodo);
+    } else throw new Error("Failed to get editTodoItem");
+  };
+
+  const handleToggleIsDone = (todo: Todo) => {
+    const updatedTodo: Todo = { ...todo, isDone: !todo.isDone };
+    setTodos(
+      todos.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo))
+    );
+  };
+
+  const handleUpdateTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!editingTodo || editingTodo?.title.trim() === "")
+      throw new Error("Failed to update todo...");
+
+    setTodos(
+      todos.map((todo) => (todo.id === editingTodo?.id ? editingTodo : todo))
+    );
+    setIsModalOpen(false);
+  };
+
+  const handleDeleteTodo = (id: string) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos([...updatedTodos]);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todos,
+        newTodoTitle,
+        editingTodo,
+        isModalOpen,
+        handleChangeTodoTitle,
+        handleAddNewTodo,
+        handleOpenEditTodoModal,
+        handleToggleIsDone,
+        handleUpdateTodo,
+        handleDeleteTodo,
+        closeModal,
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export const useTodoContext = () => {
+  const context = useContext(TodoContext);
+  if (!context) throw new Error("Provide correct context");
+
+  return context;
+};
