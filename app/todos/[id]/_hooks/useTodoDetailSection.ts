1294ed0e8eacfea1216f591221422aedc9bd702c
KEENTO <handball0321@gmail.com>
2024-04-19 10:13:07 +0900

    Fix: finished displaying todo in TodoDetail page

--- app/todos/[id]/_hooks/useTodoDetailSection.ts
+++ app/todos/[id]/_hooks/useTodoDetailSection.ts
@@ -1,10 +1,13 @@
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter, redirect } from "next/navigation";
 
 export const useTodoDetailSection = () => {
+  const router = useRouter();
   const pathname = usePathname();
   const path = pathname.split("/")[pathname.split("/").length - 1];
 
   return {
+    router,
+    redirect,
     path,
   };
 };
