5d0dd0da3954e5d5703cd5332e4c19c19ac675c2
KEENTO <handball0321@gmail.com>
2024-05-09 08:02:09 +0900

    Fix: pathes again and again

--- app/components/ui/modal/modal/Modal.tsx
+++ app/components/ui/modal/modal/Modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import styles from "@/app/_components/ui/modal/modal/modal.module.css";
+import styles from "@/app/components/ui/modal/modal/modal.module.css";
 import { ReactNode } from "react";
 
 type ModalProps = {
